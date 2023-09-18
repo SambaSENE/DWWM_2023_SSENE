1921a67e3a10d7b3b372d577f7534d2becbe522e
samba sene <80283818+SambaSENE@users.noreply.github.com>
2023-09-18 22:18:09 +0200

    ADD

--- JS/Exercice_1/js/main.js
+++ JS/Exercice_1/js/main.js
@@ -1,15 +1,19 @@
-function afficher(){
+function afficher() {
   let dateJour = new Date();
   let annee = dateJour.getFullYear();
-  let mois = dateJour.getFullMonth()+1;
-  let jour = dateJour.getFullDate();
+  let mois = dateJour.getMonth() + 1;
+  let jour = dateJour.getUTCDate();
 
   mois = (mois < 10) ? '0' + mois : mois;
-  jour = (jour < 10) ? '0' + jour : jour
-  let chaineDate = jour +'/' + mois + '/' + annee
+  jour = (jour < 10) ? '0' + jour : jour;
+  let chaineDate = jour + '/' + mois + '/' + annee;
 
 
+  document.getElementById("date").value = chaineDate;
 }
-document.querySelector('#btnDate').addEventListener()
 
+
+document.querySelector("#btnDate").addEventListener("click", function () {
+  afficher();
+})
 
