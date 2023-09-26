ad928a89e8d602aeb4d03cb1c253e45855fc1514
Samba SENE <80283818+SambaSENE@users.noreply.github.com>
2023-09-26 16:55:07 +0200

    add

--- JS/Exercice_3/js/main.js
+++ JS/Exercice_3/js/main.js
@@ -1,38 +1,48 @@
 function valNum(maChaine) {
-    //maChaine = maChaine.toUpperCase();
+    maChaine = maChaine.toUpperCase();
     let somme = 0;
     for (let i = 0; i < maChaine.length; i++) {
-        const charCode = maChaine.charCodeAt();
-
+        let charCode = maChaine.charCodeAt();
         if (charCode >= 65 && charCode <= 90) {
             somme += charCode - 64;
         }
-
-
     }
     return somme;
 }
 
-// function calculerSigne(moisNaissance) {
-//     let signeAstro = ["Capricorne", "Verseau", "Poisson", "Belier", "Taureau", "Gémeaux", "Cancer", "Lion", "Vierge", "Balance", "Scorpion", "Sagittaire"]
-//     moisNaissance = document.getElementById(moisNaissance);
-//     let date = new Date();
-//     moisNaissance = date.getMonth();
-//     for (let i = 0; i < signeAstro.length; i++) {
-//         if (signeAstro[i] == moisNaissance.value) {
-//             return signeAstro[moisNaissance];
-//         }
-//     }
 
 
-// }
-function calculerSigne(moisNaissance) {
-    const signes = ["Capricorne","Verseau","Poisson","Bélier","Taureau","Gémeaux","Cancer","Lion","Vierge","Balance","Scorpion","Sagittaire"];
-    return signes[moisNaissance -1]; // A voir pourquoi cette erreur
+
+function calculerSigne(_moisNaissance) {
+
+    mois = _moisNaissance;
+    maChaine = '';
+    let signesAstro = ["Capricorne", "Verseau", "Poisson", "Belier", "Taureau", "Gémeaux", "Cancer", "Lion", "Vierge", "Balance", "Scorpion", "Sagittaire"];
+
+    maChaine = signesAstro[mois];
+
+    return maChaine;
+
 }
+function recupMois() {
+    dateNaissance = document.querySelector('#dateNaissance').value;
+    let date = new Date(dateNaissance);
 
-let mois = document.querySelector("#mois");
-mois.addEventListener('click', () => {
-    calculerSigne(mois.value);
+    let mois = date.getMonth();
+    return mois
+}
+document.querySelector("#dateNaissance").addEventListener('blur', () => {
+    console.log(calculerSigne(recupMois()))
+})
+function formOK() {
+    let nom = document.querySelector("#nomUtilisateur").value;
+    let prenom = document.querySelector("#prenomUtilisateur").value;
+    let jour = document.querySelector("#jour").value;
+    let mois = document.querySelector("#mois").value;
+    let annee = document.querySelector("#annee").value;
 
-});
+    if (nom !== "" && prenom !== "" && jour !== "" && mois !== "" && annee !== "") {
+        return true;
+    }
+    return false;
+}
