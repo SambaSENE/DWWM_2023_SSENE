051cc5a780bc65a67046389616a41b37a944ec4a
Samba SENE <80283818+SambaSENE@users.noreply.github.com>
2023-09-28 12:27:25 +0200

    add

--- JS/Exercice_3/js/main.js
+++ JS/Exercice_3/js/main.js
@@ -1,16 +1,47 @@
-function valNum(_champ) {
-     chaine = document.querySelector('#' + _champ).value.toUpperCase();
-    let somme = 0;
+function createOptionJour(){
+    let option = document.createElement('option');
+    option.value = 0;
+    option.text = 'jour'
+    document.querySelector('#jour').appendChild(option);
+    
+    for (let i = 1; i < 32; i++) {
+        option = document.createElement('option');
+        option.value = i;
+        option.text =i;
+        document.querySelector('#jour').appendChild(option);
+        
+    }
 
+}
+createOptionJour();
+
+function createOptionMois(){
+    let option = document.createElement('option');
+    option.value = 0;
+    option.text = 'Mois';
+    document.querySelector('#mois').appendChild(option);
+
+    for (let i = 1; i < 13 ;i++) {
+        option = document.createElement('option');
+        option.value = i;
+        option.text = i;
+        document.querySelector('#mois').appendChild(option);
+        
+    }
+
+}
+createOptionMois();
+
+
+function valNum(_champ) {
+    let chaine = (document.getElementById(_champ).value).toUpperCase();
+    var somme = 0;
+    console.log(chaine);
     for (let i = 0; i < chaine.length; i++) {
         let charCode = chaine.charCodeAt(i);
-
-        if (charCode >= 65 && charCode <= 90) {
-            somme += charCode - 64;
-        }
-
-    } 
-    console.log(chaine);
+        somme += charCode - 64;
+       
+    }
     return somme;
 }
 
@@ -27,6 +58,8 @@ function calculerSigne(_moisNaissance) {
 
     let chaine = '';
     let signe = ["Capricorne", "Verseau", "Poisson", "Belier", "Taureau", "Gémeaux", "Cancer", "Lion", "Vierge", "Balance", "Scorpion", "Sagittaire"];
+
+
     chaine = signe[_moisNaissance - 1];
 
     return chaine;
@@ -47,8 +80,8 @@ function verifierFormulaire() {
     let nomUtilisateur = document.querySelector('#nomUtilisateur').value;
     let prenomUtilisateur = document.querySelector('#prenomUtilisateur').value;
     let moisNaissance = document.querySelector('#mois').value;
-
-    if (nomUtilisateur === '' || prenomUtilisateur === '' || moisNaissance === '') {
+    let jour = document.querySelector('#jour').value;
+    if (nomUtilisateur !== '' && prenomUtilisateur !== '' && moisNaissance !== '' && jour !== '') {
         return true;
     } else {
         return false;
@@ -70,22 +103,35 @@ function calculerPseudo() {
     let nom = document.querySelector('#nomUtilisateur').value;
     let prenom = document.querySelector('#prenomUtilisateur').value;
     let mois = document.querySelector('#mois').value;
-    let pseudo = '';
+
     if (nom !== '' && prenom !== '' && mois !== '') {
         let signe = calculerSigne(mois);
-        let valNumNom = valNum(nom);
-        let valNumPrenom = valNum(prenom);
+        nom = valNum('nomUtilisateur');
+        prenom = valNum('prenomUtilisateur');
 
-        pseudo = signe + (valNumNom + valNumPrenom);
+        let pseudo = signe + (nom + prenom);
         document.querySelector('#pseudo').value = pseudo;
 
 
-
+        console.log(pseudo)
     }
-    return pseudo;
+
 }
 
 
-document.querySelector('#pseudo').addEventListener('blur', () => {
+document.querySelector('#prenomUtilisateur').addEventListener('blur', () => {
+    calculerPseudo();
+});document.querySelector('#nomUtilisateur').addEventListener('blur', () => {
     calculerPseudo();
 });
+document.querySelector('#mois').addEventListener('change', () => {
+    calculerPseudo();
+});
+
+function newCookie(_nom ,  _value){
+    let dateJour = new Date();
+    let dateExpire = new Date(dateJour.getFullYear() , dateJour.getMonth() + 1 , dateJour.getDate());
+    
+    dateExpire = dateExpire.toUTCString();
+
+}
