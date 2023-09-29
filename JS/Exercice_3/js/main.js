db5d90e6be3268536923eb254771e8cf2bc3cf14
Samba SENE <80283818+SambaSENE@users.noreply.github.com>
2023-09-29 11:52:29 +0200

    add
    create option jour [Bug]!!!!!!!!!!!!

--- JS/Exercice_3/js/main.js
+++ JS/Exercice_3/js/main.js
@@ -1,34 +1,22 @@
 function createOptionJour() {
-    let mois = document.querySelector('#mois').value;
-    let joursDansMois = 31; 
-
-    if (mois === '2') { 
-        joursDansMois = 28; 
-    } else if (['4', '6', '9', '11'].includes(mois)) { 
-        joursDansMois = 30;
+    let uneOption = document.createElement('option');
+    uneOption.value = 0;
+    uneOption.text = "choisissez votre jour";
+    // document.querySelector("#jour").options[0] = uneOption;
+    document.querySelector("#jour").appendChild(uneOption);
+    for (let i = 1; i < 32; i++) {
+        uneOption = document.createElement('option');
+        uneOption.value = i;
+        uneOption.text = i;
+        document.querySelector("#jour").options[i] = uneOption;
     }
 
-    let selectJour = document.querySelector('#jour');
-    selectJour.innerHTML = ''; 
-
-    let option = document.createElement('option');
-    option.value = 0;
-    option.text = 'jour';
-    selectJour.appendChild(option);
-
-    for (let i = 1; i <= joursDansMois; i++) {
-        option = document.createElement('option');
-        option.value = i;
-        option.text = i;
-        selectJour.appendChild(option);
-    }
 }
-
 createOptionJour();
 
 function createOptionMois() {
     let selectMois = document.querySelector('#mois');
-    selectMois.innerHTML = ''; 
+    selectMois.innerHTML = '';
 
     let option = document.createElement('option');
     option.value = 0;
@@ -40,10 +28,10 @@ function createOptionMois() {
         option.value = i;
         option.text = i;
 
-       
+
         if (i === 2) {
             option.selected = true;
-            createOptionJour(); 
+            createOptionJour();
         }
 
         selectMois.appendChild(option);
@@ -60,7 +48,7 @@ function valNum(_champ) {
     for (let i = 0; i < chaine.length; i++) {
         let charCode = chaine.charCodeAt(i);
         somme += charCode - 64;
-       
+
     }
     return somme;
 }
@@ -141,55 +129,46 @@ function calculerPseudo() {
 
 document.querySelector('#prenomUtilisateur').addEventListener('blur', () => {
     calculerPseudo();
-});document.querySelector('#nomUtilisateur').addEventListener('blur', () => {
+}); document.querySelector('#nomUtilisateur').addEventListener('blur', () => {
     calculerPseudo();
 });
 document.querySelector('#mois').addEventListener('change', () => {
     calculerPseudo();
 });
 
-function newCookie(_nom ,  _value){
+function newCookie(_nom, _value) {
     let dateJour = new Date();
-    let dateExpire = new Date(dateJour.getFullYear() , dateJour.getMonth() + 1 , dateJour.getDate());
+    let dateExpire = new Date(dateJour.getFullYear(), dateJour.getMonth() + 1, dateJour.getDate());
     dateExpire = dateExpire.toUTCString();
-    
-    document.cookie = _nom + '=' + _value + '; expires = ' + dateExpire + '; SameSite = lax';   
+
+    document.cookie = _nom + '=' + _value + '; expires = ' + dateExpire + '; SameSite = lax';
 }
 
-document.querySelector('#valider').addEventListener('click' , ()=>{
-    newCookie('nom' ,  document.querySelector('#nomUtilisateur').value);
+document.querySelector('#valider').addEventListener('click', () => {
+    newCookie('nom', document.querySelector('#nomUtilisateur').value);
     window.location.href = 'accueil.html';
 });
-function nbJrsRestants(_jour , _mois , _annee){
+
+function nbJrsRestants(_jour, _mois, _annee) {
     let dateActu = new Date();
-    let annviDate = new Date(dateActu.getUTCFullYear , _mois , _jour);
+    let annviDate = new Date(dateActu.getUTCFullYear, _mois, _jour);
 
     let diffTemps = annviDate - dateActu;
 
-    if(diffTemps < 0){
-        annviDate = new Date(dateActu.getFullYear() + 1, _mois -1 , _jour);
+    if (diffTemps < 0) {
+        annviDate = new Date(dateActu.getFullYear() + 1, _mois - 1, _jour);
         diffTemps = annviDate - dateActu;
     }
-    let joursRestants = Math.ceil(diffTemps / (1000 * 60 *60 * 24));
+    let joursRestants = Math.ceil(diffTemps / (1000 * 60 * 60 * 24));
     return joursRestants;
-    
+
 }
-window.onload = function() {
-    let nom = getCookie("nom");
-    let prenom = getCookie("prenom");
-    let jour = getCookie("jour");
-    let mois = getCookie("mois");
-    let annee = getCookie("annee");
-    let pseudo = getCookie("pseudo");
+let annee = document.querySelector('#annee').value;
+let mois = document.querySelector('#mois').value;
+let jour = document.querySelector('#jour').value;
 
-    document.querySelector("#nomUtilisateur").textContent = nom;
-    document.querySelector("#prenomUtilisateur").textContent = prenom;
-    document.querySelector("#dateNaissance").textContent = `${jour}/${mois}/${annee}`;
-    document.querySelector("#pseudo").textContent = pseudo;
+document.querySelector('#annee').addEventListener('change', () => {
+    nbJrsRestants(annee, mois, jour);
+})
 
-    // Calculez le nombre de jours restants jusqu'à l'anniversaire de l'utilisateur
-    let joursRestants = nbJrsRestants(jour, mois, new Date().getFullYear());
 
-    // Mettez à jour l'élément HTML avec le nombre de jours restants
-    document.querySelector('#nbjoursRestants').textContent = joursRestants;
-};
