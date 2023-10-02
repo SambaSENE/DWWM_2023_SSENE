ca83df4d9a086d9a3d3bd7e93317155e18550194
samba sene <80283818+SambaSENE@users.noreply.github.com>
2023-10-02 21:52:20 +0200

    add

--- JS/Exercice_3/js/main2.js
+++ JS/Exercice_3/js/main2.js
@@ -1,28 +1,32 @@
-// genere les options dans inscriptionAsto.html
-function genererJoursDuMois() {
-    // Obtenir une référence à l'élément select
-    let select = document.querySelector('#jour');
+function afficheJour() {
+    let option = document.createElement('option');
+    option.value = 0;
+    option.text = "choisissez votre jour";
+    document.querySelector("#jour").options[0] = option;
+    //document.querySelector('#jour').appendChild(option);
 
-    // Créer une date pour le mois actuel
-    let date = new Date();
-
-    // Obtenir le nombre de jours dans le mois
-    let dernierJour = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-
-    // Boucle pour ajouter les options pour les jours du mois
-    for (let i = 1; i <= dernierJour; i++) {
-        let option = document.createElement('option');
+    for (let i = 1; i < 32; i++) {
+        option = document.createElement('option');
+        option.value = i;
         option.text = i;
-        select.appendChild(option);
-        //·select.add(option);
-        //document.querySelector('#jour').option[i]=option;
+        document.querySelector('#jour').options[i]=option;
 
     }
+
 }
 
-genererJoursDuMois();
 
-function genererMois() {
+document.querySelector('#jour').addEventListener('blur', ()=>{
+
+    afficheJour();
+})
+    
+
+
+//createOptionJour();
+
+
+function createOptionMois() {
     let selectMois = document.querySelector('#mois');
     selectMois.innerHTML = '';
 
@@ -37,82 +41,83 @@ function genererMois() {
         option.text = i;
 
 
-        //if (i === 2) {
-        //  option.selected = true;
-
-        //}
+        if (i === 2) {
+            option.selected = true;
+        afficheJour();
+        }
 
         selectMois.appendChild(option);
     }
 }
 
-genererMois();
+createOptionMois();
 
-function genererAnnees() {
-    // Obtenir une référence à l'élément select
-    var select = document.getElementById("annee");
-
-    // Année de début et de fin
-    var anneeDebut = 1900;
-    var anneeFin = 2023;
-
-    // Boucle pour ajouter les options pour les années
-    for (var annee = anneeDebut; annee <= anneeFin; annee++) {
-        var option = document.createElement("option");
-        option.text = annee;
-        select.add(option);
-    }
-}
-genererAnnees();
 
 function valNum(_champ) {
-    let chaine = (document.querySelector('#' + _champ).value).toUpperCase();
-
-    let somme = 0;
+    let chaine = (document.getElementById(_champ).value).toUpperCase();
+    var somme = 0;
     for (let i = 0; i < chaine.length; i++) {
         let charCode = chaine.charCodeAt(i);
         somme += charCode - 64;
+
     }
+    // console.log(somme);
     return somme;
 }
 
-function calculerSigne(_moisNaissance) {
-    let signe = ["Capricorne", "Verseau", "Poisson", "Bélier", "Taureau", "Gémeaux", "Cancer", "Lion", "Vierge", "Balance", "Scorpion", "Sagittaire"];
-    return signe[_moisNaissance - 1];
-}
+document.querySelector('#nomUtilisateur').addEventListener('blur', () => {
+    valNum('nomUtilisateur');
+});
+document.querySelector('#prenomUtilisateur').addEventListener('blur', () => {
+    valNum('prenomUtilisateur');
+});
 
+
+
+function calculerSigne(_moisNaissance) {
+
+    let chaine = '';
+    let signe = ["Capricorne", "Verseau", "Poisson", "Belier", "Taureau", "Gémeaux", "Cancer", "Lion", "Vierge", "Balance", "Scorpion", "Sagittaire"];
+
+
+    chaine = signe[_moisNaissance - 1];
+
+    return chaine;
+}
 function recupMois(_mois) {
     let moisNaissance = document.querySelector('#' + _mois).value;
+
     return moisNaissance;
 }
 
+document.querySelector('#mois').addEventListener('blur', () => {
+    console.log(calculerSigne(recupMois('mois')));
+});
+
+
 function verifierFormulaire() {
+
     let nomUtilisateur = document.querySelector('#nomUtilisateur').value;
     let prenomUtilisateur = document.querySelector('#prenomUtilisateur').value;
     let moisNaissance = document.querySelector('#mois').value;
     let jour = document.querySelector('#jour').value;
-
-    // Vérifiez les conditions pour activer/désactiver le bouton
     if (nomUtilisateur !== '' && prenomUtilisateur !== '' && moisNaissance !== '' && jour !== '') {
-        document.querySelector('#valider').disabled = false; // Activez le bouton
+
+        return true;
     } else {
-        document.querySelector('#valider').disabled = true; // Désactivez le bouton
+        return false;
     }
 }
 
-document.querySelector('#valider').addEventListener('click', verifierFormulaire);
-
 
 document.querySelector('#nomUtilisateur').addEventListener('blur', () => {
-    console.log(valNum('nomUtilisateur'));
+    verifierFormulaire();
 });
-
 document.querySelector('#prenomUtilisateur').addEventListener('blur', () => {
-    console.log(valNum('prenomUtilisateur'));
+    verifierFormulaire();
 });
-
 document.querySelector('#mois').addEventListener('blur', () => {
-    console.log(calculerSigne(recupMois('mois')));
+    verifierFormulaire();
 });
 
 function calculerPseudo() {
@@ -128,10 +133,10 @@ function calculerPseudo() {
 
         let pseudo = signe + prenom + (nom + prenom);
 
-
+      
         //let compNom = document.querySelector('#nomUtilisateur').value;
         //let compPrenom =  document.querySelector('#prenomUtilisteur').value;
-        // let composPseudo = 
+       // let composPseudo = 
 
         document.querySelector('#pseudo').value = pseudo;
 
@@ -172,6 +177,7 @@ function newCookie(_nom, _value) {
 
     document.cookie = _nom + '=' + _value + '; expires = ' + dateExpire + '; SameSite = lax';
 }
+
 document.querySelector('#annee').addEventListener('change', () => {
     nbJrsRestants(annee);
 })
@@ -188,31 +194,5 @@ document.querySelector('#valider').addEventListener('click', () => {
     newCookie('prenom', document.querySelector('#prenomUtilisateur').value);
     window.location.href = 'accueil.html';
 });
-
-function afficherCookies() {
-    var cookies = document.cookie.split(';'); // Obtenir tous les cookies et les séparer
-    var tableBody = document.getElementById('cookieTable');
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i].trim(); // Supprimer les espaces autour du cookie
-        var cookieParts = cookie.split('=');
-        var nom = cookieParts[0];
-        var valeur = cookieParts[1];
-
-        var newRow = document.createElement('tr');
-        var nomCell = document.createElement('td');
-        var valeurCell = document.createElement('td');
-
-        nomCell.textContent = nom;
-        valeurCell.textContent = valeur;
-
-        newRow.appendChild(nomCell);
-        newRow.appendChild(valeurCell);
-
-        tableBody.appendChild(newRow);
-    }
-}
-window.onload = afficherCookies;
-// genere le trableau dans accueil.html
 
 
