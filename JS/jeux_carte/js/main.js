67b6bfcfc5d8375765845b09c481d3763117f273
samba sene <80283818+SambaSENE@users.noreply.github.com>
2023-10-05 23:04:10 +0200

    add

--- JS/jeux_carte/js/main.js
+++ JS/jeux_carte/js/main.js
@@ -1,67 +1,60 @@
-// function  addCell(tr ,  text){
-//     let td = tr.insertCell();
-//     td.textContent = text;
-//     return td;
-// }
-// function titleCell(row ,  text){
-//     let cell = document.createElement('th');
-//     cell.textContent = text
-//     row.appendChild(cell);
-// }
+let table = document.createElement('table');
+document.body.appendChild(table);
 
-// function createCell(row , object){
-//     for (const key in object) {
-//         addCell(row , object[key]);
-//     }
-// }
-
-
-// let newRow = document.createElement('tr');
-// let nomCell = document.createElement('td');
-// nomCell.textContent = 'Nom utilisateur';
-// let valeurCell = document.createElement('td');
-// valeurCell.textContent = afficherCookies('nom');
-// newRow.appendChild(nomCell);
-// newRow.appendChild(valeurCell);
-// document.querySelector('#cookieTable').appendChild(newRow);
-
-function createHead(row ,key) {
-    
-    let row = document.createElement('th');
-    cell.textContent = key;
-    row.appenChild(row)
-
+function createHead(value) {
+    let th = document.createElement('th');
+    th.textContent = value;
+    return th;
 }
-function createBody(row , value){
-    let cell = document.createElement('td');
-    row.textContent =  value;
-    cell.appendChild()
+
+function createBody(value) {
+    let td = document.createElement('td');
+    td.textContent = value;
+    return td;
 }
-// Je  cree une Ojbet XHR
 
 let xhr = new XMLHttpRequest();
-
-// j'initialise la requte avec open()
 xhr.open("GET", "https://arfp.github.io/tp/web/html-css-js/02-cardgame/cardgame.json");
-
-// je convertis en formation JSON
 xhr.responseType = 'json';
-// envoie de la requete 
 xhr.send();
-// 
 
-// j'affiche des que la reponse est disponible
 xhr.onload = () => {
-    if (xhr.status != 200) {
-        console.log('Erreur' + xhr.status + xhr.statusText);
+    if (xhr.status !== 200) {
+        console.log('Erreur ' + xhr.status + ' ' + xhr.statusText);
     } else {
         let container = xhr.response;
 
-        for (const key in container) {
-            console.log(container);
-           
+        // Crée une ligne d'en-tête <tr>
+        let headerRow = document.createElement('tr');
+        if (container.length > 0) {
+            let firstItem = container[0];
+            for (let key in firstItem) {
+                // Ajoute un en-tête de tableau <th>
+                let th = createHead(key);
+                headerRow.appendChild(th);
+                console.log('element');
+
+            }
+            // Ajoute la ligne d'en-tête au tableau
+            table.appendChild(headerRow);
         }
-        // // fillTable(container);
+
+        // Crée des lignes de données pour chaque élément du container
+        for (let item of container) {
+            let dataRow = document.createElement('tr');
+            for (let key in item) {
+                if (key == 'description') {
+                    // Ajoute une cellule de données <td>
+                    let td = createBody(item[key]);
+                    dataRow.appendChild(td);
+                } else {
+                    
+                    let td = createBody(item[key]);
+                    dataRow.appendChild(td);
+                }
+            }
+            // Ajoute la ligne de données au tableau
+            table.appendChild(dataRow);
+        }
     }
 }
-
