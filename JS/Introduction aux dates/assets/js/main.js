a10b712bc0e9c41a8176ea59440bec28d4951c66
Samba SENE <80283818+SambaSENE@users.noreply.github.com>
2023-10-11 16:55:54 +0200

    add

--- JS/Introduction aux dates/assets/js/main.js
+++ JS/Introduction aux dates/assets/js/main.js
@@ -2,29 +2,46 @@ const dateNaissance = document.getElementById('dateNaissance');
 var btnCalculer = document.getElementById('calculer');
 const resultat = document.getElementById('resultat');
 
-var date ;
+var date;
 var heure;
 
 //  traitement
-function verifierForm(){
+function verifierForm() {
     return (dateNaissance.value !== '');
 }
 
 
-function afficherDateNaissance(){
-             date = new Date(dateNaissance.value);
-            return date.toLocaleDateString('fr')  ;      
-    
-} 
-function afficherHeure(){
+function afficherDateNaissance() {
+    date = new Date(dateNaissance.value);
+    return date.toLocaleDateString('fr');
+
+}
+function afficherHeure() {
     heure = new Date(dateNaissance.value);
     return heure.toLocaleTimeString();
 }
 
+
+/**
+ * 
+ * @returns 
+ */
+
+function calculerNbAnnee() {
+    let date = new Date();
+    date.getFullYear();
+
+   
+
+}
+calculerNbAnnee();
+function calculerSignesAstro() {
+
+}
 // Affichage
 btnCalculer = document.addEventListener('click', () => {
     if (verifierForm()) {
-        resultat.innerText = afficherDateNaissance() +' ' + afficherHeure();
+        resultat.innerText = afficherDateNaissance() + ' ' + afficherHeure() + ' ' + calculerNbAnnee();
     } else {
         resultat.innerText = 'pas ok';
     }
---
+++ JS/LesClasses/assets/js/mesClasses/Maclasse.js
@@ -0,0 +1,6 @@
+// class Astrologie{
+
+//     constructor(signe){
+//         this.signe = ["Capricorne", "Verseau", "Poisson", "Bélier", "Taureau", "Gémeaux", "Cancer", "Lion", "Vierge", "Balance", "Scorpion", "Sagittaire"];
+//     }
+// }
