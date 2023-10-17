a2ec45a59942a3e725f0a542526bfac8a90fd520
Samba SENE <80283818+SambaSENE@users.noreply.github.com>
2023-10-17 09:18:53 +0200

    add

--- JS/intervalles_temporelles/assets/js/MaDate.js
+++ JS/intervalles_temporelles/assets/js/MaDate.js
@@ -1,20 +1,49 @@
-class MaDate
-{
-    /**
-     * 
-     * @param {Date} _date  date
+class MaDate{
+    /*
+     * le constructeur
+     * @param {Date} _date la date saisie  
      */
     constructor(_date)
     {
-        this.currentDate  =  new Date();
-        this.datePasse = _date;
+        this.currentDate  = new Date();
+        this.passe = _date;
+    }
+    /**
+     * retoune vrai si la date est dans le passe
+     * @returns {Boolean} Vrai si date est passe
+     */
+    estDansLePasse()
+    {
+        return  this.passe < this.currentDate ;
     }
 
-  
-    retouneDateCourante()
+    /**
+     * retoune la date d'aujourd'hui 
+     * @returns {Date} la date d'aujourd'hui
+     */
+    returnDate()
     {
-        
-        return this.currentDate ;
+        return this.currentDate.toLocaleDateString('fr');
+    }
+
+    /**
+     * retourne l'heure actuelle 
+     * @returns {Date} l'heure actuelle
+     */
+    returnHour()
+    {
+        return this.currentDate.toLocaleTimeString();
+    }
+
+    /**
+     * 
+     * @returns {Date}
+     */
+    returnInter()
+    {
+        return this.currentDate - this.passe
     }
 }
-export { MaDate };
+
+export { MaDate }
+>>>>>>> Stashed changes
--- JS/intervalles_temporelles/assets/js/main.js
+++ JS/intervalles_temporelles/assets/js/main.js
@@ -1,17 +1,28 @@
-import { MaDate  } from "./MaDate.js";
+
+// Importation des classes
+import { MaDate } from "./MaDate.js";
+// Declaration
 const inputDateActu = document.getElementById('dateActu');
 const inputHeureActu = document.getElementById('heureActu');
-const btnValider =  document.getElementById('valider');
-const afficheHeureDate =  document.getElementById('dateHeureJour');
+const btnValider = document.getElementById('valider');
+const dateDuJour = document.getElementById('dateDuJour');
+const inputDateInter = document.getElementById('dateInterv');
+const btnCalculer = document.getElementById('calculer');
+const nbjoursInter = document.getElementById('nbJoursInter');
 
-const inputDateInter = document.getElementById('dateInter');
+
 const btnCalculerInter = document.getElementById('calculer');
 const afficherDateInter =  document.getElementById('dateIntervale');
 
-btnValider.addEventListener('click' ,  ()=>{
-    let dateCourante = 
-    afficheHeureDate.innerHTML = MaDate.retourneDateCourante();
-});
-btnCalculerInter.addEventListener('click' , ()=>{
-    console.log('ok');
+
+btnValider.addEventListener('click' , ()=>{
+    let maDate = new MaDate(inputDateActu.value);
+    dateDuJour.innerHTML = 'hello ';
+    if (maDate.estDansLePasse()) {
+        dateDuJour.innerHTML = `Aujourd'hui nous sommes le <span>` + maDate.returnDate() + `</span> l'heure courante est :  ` + '<span>' + maDate.returnHour() + '</span>';
+    }else{
+        return false;
+    }
+    
 })
+
