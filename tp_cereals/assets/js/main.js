c5606bf15acfd99d558d8a64dae5a2e10940181f
Samba SENE <80283818+SambaSENE@users.noreply.github.com>
2023-11-09 15:24:48 +0100

    add
    Cereals (delete item)

--- tp_cereals/assets/js/main.js
+++ tp_cereals/assets/js/main.js
@@ -9,6 +9,10 @@ const myApp = {
             dataSouce: [],// données source
             cereals: [],// copie 
             nutriscore: [],
+            arrayKey: [],
+            dataCol: [],
+
+
         }
     },
     async mounted() {
@@ -17,14 +21,45 @@ const myApp = {
             let newCereal = new Cereal(data)
             this.cereals.push(newCereal);
             this.cereals.push(newCereal);
-            this.nutriscore.push(Math.round(newCereal.rating))
+            this.nutriscore.push(Math.round(newCereal.rating));
         }
-
+        for (const key in this.cereals[0]) {
+            this.arrayKey.push(key)
+        }
+        console.log(this.arrayKey);
         this.dataSouce.sort();
     }, methods: {
-        nutriScore(){
-            
+        sortCol(event) {
+            let attribut = event.target.dataset.attribut;
+            if (attribut) {
+
+                this.cereals.sort((a, b) => {
+                    if (a[attribut] > b[attribut]) {
+                        return 1;
+                    }
+                    else if (a[attribut] < b[attribut]) {
+                        return -1;
+                    }
+                    return 0;
+                }).reverse()
+            } else {
+                this.cereals.sort((a, b) => {
+                    if (a[attribut] > b[attribut]) {
+                        return 1;
+                    }
+                    else if (a[attribut] < b[attribut]) {
+                        return -1;
+                    }
+                    return 0;
+                })
+            }
+        },
+        deleteElement(event){
+            let id= event.target.dataset.cereal.id ;
+            this.cereals.filter((id) => this.cereals !== id);
         }
     }
 }
 createApp(myApp).mount('#app');
+
+
