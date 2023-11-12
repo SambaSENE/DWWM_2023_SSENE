8eb0884d767e375d1e89e796dcd26edc59d828ef
samba sene <80283818+SambaSENE@users.noreply.github.com>
2023-11-12 17:30:45 +0100

    add
    cereals search() ::  bug fix😔😔😔

--- tp_cereals/assets/js/main.js
+++ tp_cereals/assets/js/main.js
@@ -8,9 +8,9 @@ const myApp = {
         return {
             dataSouce: [],// données source
             cereals: [],// copie 
+            caloriesColumn: [],
             asc: true,
-            search: '',
-
+            searchCereals: '',
         }
     },
     async mounted() {
@@ -19,40 +19,64 @@ const myApp = {
             let cereal = new Cereal(data)
             this.dataSouce.push(cereal);
         }
-
-        this.cereals = [...this.dataSouce]
-
         this.dataSouce.sort();
-    }, methods: {
-        sortCol(event) {
-            let attribut = event.target.attribut;
-            if (attribut) {
 
-                this.cereals.sort((a, b) => {
-                    if (a[attribut] > b[attribut]) {
-                        return 1;
-                    }
-                    else if (a[attribut] < b[attribut]) {
-                        return -1;
-                    }else {
-                        return 0;
-                    }
-                })
-                
-            } 
+        this.cereals = [...this.dataSouce];
+        console.log(this.cereals);
+        for (const item of this.cereals) {
+            let elementInt = parseInt(item.calories);
+            this.caloriesColumn.push(elementInt)
+        }
+
+
+    }, computed: {
+        averageCalories() {
+            let total = this.caloriesColumn.reduce((a, b) => a + b, 0);
+            return (total / this.cereals.length).toFixed(2);
+        }, 
+        nbElement(){
+            return this.cereals.length;
+        }
+    }
+
+    , methods: {
+        searchEvent(){
+             this.cereals = this.cereals.filter(cereal => cereal.name.toLowerCase().includes((this.searchCereals.toLowerCase()).trim()));
+
+        }, 
+        sortCol(event) {
+            console.log(event.target.id);
+            // event.target.id = attribut "id" de l'élément cible de l'évènement
+            let attribut = event.target.id;
+
+            // fonction de tri
+            let fonctiontri = (a, b) => {
+                if (a[attribut] > b[attribut]) {
+                    return 1;
+                }
+                else if (a[attribut] < b[attribut]) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+
+            // tri des données
+            this.cereals.sort(fonctiontri);
+
+            // si la valeur de "asc" est "false", on inverse le tri
+            if (this.asc === false) {
+                this.cereals.reverse();
+            }
+
+            // inversion du sens du tri pour le prochain clic
+            this.asc = !this.asc;
         },
         deleteElement(event) {
             const idCereal = event.target.dataset.id;
             this.cereals = this.cereals.filter(cereal => cereal.id != idCereal);
-        },
-        searchCereals() {
-            // this.cereals = this.cereals.find(cereal => )
         }
-    }, computed: {
-        averageCalories() {
-            const total = this.cereals.reduce((currentValue, acumulator) => currentValue + acumulator, 0)
-        }
-    }
+    },
 }
 createApp(myApp).mount('#app');
 
