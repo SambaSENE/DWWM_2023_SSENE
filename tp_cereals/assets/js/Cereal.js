f44dec0fd96a413a2a56687a6c5eb1482673bbcc
Samba SENE <80283818+SambaSENE@users.noreply.github.com>
2023-11-13 16:41:00 +0100

    add
    céréales

--- tp_cereals/assets/js/Cereal.js
+++ tp_cereals/assets/js/Cereal.js
@@ -2,18 +2,19 @@ class Cereal
 {
     constructor(_cereal){
         Object.assign(this,_cereal);
-        this.ns = this.nutriScores(_cereal.rating);
+        this.ns = this.nutriScoresCol(_cereal.rating);
+      
     }
 
-    nutriScores(_cereal){
+    nutriScoresCol(_cereal){
         const rating =  parseInt(_cereal);
         if(rating < 35 ){
-            return 'E';
-        }else if(rating >70 && rating > 80){
+            return  "E";
+        }else if(rating >= 70 && rating < 80){
             return 'B';
-        }else if(rating > 55 && rating < 70){
+        }else if(rating >= 55 && rating < 70){
             return 'C';
-        }else if(rating > 35 && rating < 55){
+        }else if(rating >= 35 && rating < 55){
             return 'D';
         }else {
             return 'A';
--- tp_cereals/assets/js/main.js
+++ tp_cereals/assets/js/main.js
@@ -6,47 +6,84 @@ const { createApp } = Vue;
 const myApp = {
     data() {
         return {
-            dataSouce: [],// données source
+            dataSource: [],// données source
             cereals: [],// copie 
             caloriesColumn: [],
-            asc: true,
-            searchCereals: '',
             checked: [],
+            categories: '',
+            searchCereals: '',
+            asc: true,
         }
     },
     async mounted() {
+
         const dataJson = await Db.fetchUrl(apiUrl);
         for (const data of dataJson.data) {
             let cereal = new Cereal(data)
-            this.dataSouce.push(cereal);
+            this.dataSource.push(cereal);
         }
-        this.dataSouce.sort();
+        this.dataSource.sort();
 
-        this.cereals = [...this.dataSouce];
+        this.cereals = [...this.dataSource];
         // console.log(this.cereals);
         for (const item of this.cereals) {
             let elementInt = parseInt(item.calories);
             this.caloriesColumn.push(elementInt)
         }
 
-
     }, computed: {
         averageCalories() {
             let total = this.caloriesColumn.reduce((a, b) => a + b, 0);
             return (total / this.cereals.length).toFixed(2);
-        }, 
-        nbElement(){
+        },
+        nbElement() {
             return this.cereals.length;
         }
     }
 
     , methods: {
-        searchEvent(){
-             this.cereals = this.cereals.filter(cereal => cereal.name.toLowerCase().includes((this.searchCereals.toLowerCase()).trim()));
+        /**
+         * function recherche muli-criteres
+         *      Si this.categories == 'sugar' alors
+         *         this.cereals = filtre this.dataSource sugar < 1 ;
+         *       Sinon si this.catergorie == poorSalt alors 
+         *          this.cereals = filtre this.dataSource salt < 50
+         *      sinon si this.categories == boost 
+         *          this.cereals = this.filtre dataSource vitamin >= 25 + fibre >= 10
+         *      sinon 
+         *          this.cereals     
+         * fin Si
+         * fin function
+         */
+        selectMultiCriter(){
+            if(this.nutriScore){
+                if(this.categories == 'noSugar'){
+                   this.cereals = this.dataSource.filter(cereal => cereal.sugars < 1);
+                }else if(this.categories == 'poorSault'){
+                    this.cereals = this.dataSource.filter(cereal => cereal.sodium < 50);
+                }else if(this.categories == 'boost'){
+                    this.cereals = this.dataSource.filter(cereal => cereal.vitamins >= 25  && cereal.fiber >= 10);
+                }
+                else{
+                    this.cereals;
+                }
+            }
+        },
+        nutriScore(){
+            if (this.checked.length > 0) {
+                this.cereals = this.dataSource.filter(cereal => this.checked.includes(cereal.ns));
+                
+            }else{
+                this.cereals;
+            }
+          
+        },
+        searchEvent() {
+            this.cereals = this.cereals.filter(cereal => cereal.name.toLowerCase().includes((this.searchCereals.toLowerCase()).trim()));
 
-        }, 
+        },
         sortCol(event) {
-            console.log(event.target.id);
+            
             // event.target.id = attribut "id" de l'élément cible de l'évènement
             let attribut = event.target.id;
 
